705c372c982d3eaff0376bd8b7ceed1f4c16af35
dave-home-2 <davemcelhaney@gmail.com>
2023-02-28 18:01:16 -0500

    documented sql partial update function

--- helpers/sql.js
+++ helpers/sql.js
@@ -10,11 +10,14 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
+//  creating a group of keys and a group of indexes corresponding for SQL data handling
 
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
   };
+//   Returns the data to create the references and values for SQL updates
+// combines return value with prebuilt SQL query within the patch route from ../routes/companies.js
 }
 
 module.exports = { sqlForPartialUpdate };
